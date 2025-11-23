220a39cf07d8faa135817cbf7f17a907fa552a45
griffinjoshs <leveloutusa@gmail.com>
2025-11-23 01:24:10 -0500

    contact

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { defineMiddleware } from 'astro:middleware';
-import { setSecurityHeaders } from './utils/securityHeaders';
-
-export const onRequest = defineMiddleware(async (context, next) => {
-  const response = await next();
-  setSecurityHeaders(response, context.url.pathname);
-  return response;
-});
