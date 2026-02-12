57b9fe72e2bcba674b336efed0bbba609cf89ef8
griffinjoshs <leveloutusa@gmail.com>
2026-02-12 00:49:07 -0500

    product pages

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,13 @@
 interface ImportMetaEnv {
   readonly PUBLIC_SITE_DOMAIN: string;
   readonly PUBLIC_FORMSPREE_ID?: string;
+  readonly PUBLIC_FORMSPREE_CONTACT_ID?: string;
+  readonly PUBLIC_FORMSPREE_QUOTE_ID?: string;
+  readonly PUBLIC_FORMSPREE_SUBSCRIBE_ID?: string;
+  readonly PUBLIC_FORMSPREE_WAITLIST_ID?: string;
+  readonly PUBLIC_FORMSPREE_APPLICATION_ID?: string;
+  readonly PUBLIC_FORMSPREE_MULTI_STEP_QUOTE_ID?: string;
+  readonly PUBLIC_FORMSPREE_BUDGET_TOOL_ID?: string;
   readonly PUBLIC_GOOGLE_TRANSLATE_KEY?: string;
 }
 
